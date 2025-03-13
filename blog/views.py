78771a90e93efee1a9fd83aa9917c22251f2ce04
user_name <user@email.com>
2025-03-13 16:52:05 -0400

    publication create v 1 without link

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,6 @@
-from django.views.generic import ListView, DetailView  # new
-from .models import Publication
-
+from .models import Publication  # new
+from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView
 # Create your views here.
 class PublicationListView(ListView):
     model = Publication
@@ -9,3 +9,8 @@ class PublicationListView(ListView):
 class PublicationDetailView(DetailView): # new
     model = Publication
     template_name = 'publication-detail.html'
+
+class PublicationCreateView(CreateView):
+    model = Publication 
+    template_name = 'publication-create.html'
+    fields = ['title', 'body', 'author']
