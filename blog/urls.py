cc7c16aa979c8a1f52a2b3d9c029cf4b3d094697
user_name <user@email.com>
2025-03-13 16:17:01 -0400

    publication detail done

--- blog/urls.py
+++ blog/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import PublicationListView, PublicationDetailView # new
 
 urlpatterns = [
-    path("",  PublicationListView.as_view(), name="publications-list"),
-    path("publication/<int:pk>/", PublicationDetailView.as_view(), name="publication-detail"), # new
+    path('',  PublicationListView.as_view(), name='publications-list'),
+    path('publication/<int:pk>/', PublicationDetailView.as_view(), name='publication-detail'), # new
 ]
--- blog/views.py
+++ blog/views.py
@@ -4,8 +4,8 @@ from .models import Publication
 # Create your views here.
 class PublicationListView(ListView):
     model = Publication
-    template_name = "publications-list.html"
+    template_name = 'publications-list.html'
 
 class PublicationDetailView(DetailView): # new
     model = Publication
-    template_name = "publication-detail.html"
+    template_name = 'publication-detail.html'
