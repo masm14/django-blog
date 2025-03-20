beee8ec92a7ada3184d4e99c439e8e2a8a2c25fe
user_name <user@email.com>
2025-03-20 16:36:56 -0400

    login done

--- django_project/settings.py
+++ django_project/settings.py
@@ -122,3 +122,7 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+# login redirect
+LOGIN_REDIRECT_URL = "publications-list"
--- django_project/urls.py
+++ django_project/urls.py
@@ -21,4 +21,5 @@ from django.urls import path, include # new
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("blog.urls")), # new
+    path("accounts/", include("django.contrib.auth.urls")), # new
 ]
